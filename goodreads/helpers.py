0ebaa32ae7aa23a2d8c8532c9c6165329f66f554
connieli <c.ziye.li@gmail.com>
2017-01-07 18:52:22 -0500

    it's finished it's done

--- goodreads/helpers.py
+++ goodreads/helpers.py
@@ -13,3 +13,6 @@ def table_things(**kwargs):
 def writefile(data, filepath):
     with open(filepath, 'w') as outfile:
         json.dump(data, outfile, sort_keys=True, indent=4)
+
+def split_list(list, chunk_size):
+    return [list[offs:offs+chunk_size] for offs in range(0, len(list), chunk_size)]
